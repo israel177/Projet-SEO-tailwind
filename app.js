449528be125fdfa11951d76dfd0007526ada6ebc
israel <56261401+israel177@users.noreply.github.com>
2021-03-02 11:04:15 +0100

    nav

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const menuResponsive = document.querySelector('.toggle-menu');
 const iconeToggle = document.querySelector('.icone-toggle');
+const nav = document.querySelector('.toggle');
 
 iconeToggle.addEventListener('click', () => {
 
     menuResponsive.classList.toggle('hidden');
+    nav.classList.add('hidden');
 
 })
