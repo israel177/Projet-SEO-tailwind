56e3bfe4d09d59a4ad1aeeaf4176bef545d5fdba
israel <56261401+israel177@users.noreply.github.com>
2021-03-02 12:26:53 +0100

    responsive nav + purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
+    purge: [
+        './*.html'
+    ],
     theme: {
         extend: {
             minHeight: {
                 500: "500px",
                 650: "650px"
             }
-        }
+        },
+        screens: {
+            'xs': '300px',
+            ...defaultTheme.screens,
+          }
     }
 }
